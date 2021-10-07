2c54ff5df7bc9b4a88b2755cff655c087c63e749
locdybala11 <locdybala11@gmail.com>
2021-10-07 15:08:47 +0700

    Add

--- app/js/script.js
+++ app/js/script.js
@@ -14,25 +14,37 @@ const content = [
         link:'<img src="app/img/image9.png" alt=""/>'
     },
   ]
+    var styles = "./dist/style.css";
+    var newSS=document.createElement('link');
+    newSS.rel='stylesheet';
+    newSS.href=escape(styles);
+    console.log(newSS);
+    document.getElementsByTagName("head")[0].appendChild(newSS);
+  
   console.log(content)
   const renderHtml = (data) => {
     const htmlResult = data.map(
       (obj) =>
-        ` <div class="information__item">
-        <div class="information__logo">
-            <img src="${obj.logo}" alt="">
-        </div>
-        <div  class="information__img">
-            <img src="${obj.img}" alt="">
-        </div>
-        <div class="information__title">
-            <p>${obj.title}</p>
-        </div>
-        <div class="information__link">
-            <a href="">${obj.link}</a> 
-            <p> tài trợ</p>
-        </div>
-    </div>`
+        `  <div class="container">
+            <div class="information">
+               <div class="information__item">
+                 <div class="information__logo">
+                     <img src="${obj.logo}" alt="">
+                 </div>
+                  <div  class="information__img">
+                      <img src="${obj.img}" alt="">
+                  </div>
+                  <div class="information__title">
+                      <p>${obj.title}</p>
+                  </div>
+                <div class="information__link">
+                    <a href="">${obj.link}</a> 
+                    <p> tài trợ</p>
+                </div>
+              </div>
+             </div>
+             </div>
+            `
     )
   
     return htmlResult.join('')
