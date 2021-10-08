c0cd577e98701ff81369cfde5863a3ad5e013cc5
locdybala11 <locdybala11@gmail.com>
2021-10-08 09:53:16 +0700

    add

--- app/js/script.js
+++ app/js/script.js
@@ -1,19 +1,5 @@
-const content = [
-    {
-      id: 1,
-      logo:'app/img/adxlogo2.png',
-      img:'app/img/Rectangle.png',
-      title: 'Hè này, trải nghiệm kỳ nghỉ 5 sao tại thiên đường nghỉ dưỡng Vinpearl 2021',
-      link:'thegioididong.vn'
-    },
-    {
-        id: 2,
-        logo:'app/img/adxlogo2.png',
-        img:'app/img/Rectangle.png',
-        title: 'Hè này, trải nghiệm kỳ nghỉ 5 sao tại thiên đường nghỉ dưỡng Vinpearl 2021',
-        link:'<img src="app/img/image9.png" alt=""/>'
-    },
-  ]
+import {content} from '../../dist/mock-data.js'
+
     var styles = "./dist/style.css";
     var newSS=document.createElement('link');
     newSS.rel='stylesheet';
@@ -46,7 +32,6 @@ const content = [
              </div>
             `
     )
-  
     return htmlResult.join('')
   }
   
